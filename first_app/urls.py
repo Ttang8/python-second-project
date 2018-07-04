4932b9f5bb0e6ede0aca988d59818c01d3bd446d
Terrence Tang <thtang28@gmail.com>
2018-07-03 23:52:27 -0700

    Add usersform

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,4 +3,6 @@ from first_app import views
 
 urlpatterns = [
     path('users/', views.users, name='users'),
+    path('usersform/',views.usersform,name='usersform'),
+    path('', views.index,name='index')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from first_app.models import User, AccessRecord
+
+from .forms import NewUserForm
 # Create your views here.
 
 def index(request):
-    return render(request,'first_app/index.html',name='index')
+    return render(request,'first_app/index.html')
 
 def users(request):
     user_list = User.objects.order_by('first_name')
     user_dict = {'user_list':user_list}
     # my_dict = {'insert_me':"yoooooo test"}
-    return render(request,'first_app/index.html',context=user_dict)
+    return render(request,'first_app/users.html',context=user_dict)
+
+def usersform(request):
+
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVAILD')
+
+    return render(request,'first_app/usersform.html',{'form':form})
